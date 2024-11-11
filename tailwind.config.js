d8645af50cafe0ee220e341fc89c8b1a3c60a3d6
Renate <143175623+renayoo@users.noreply.github.com>
2024-11-11 14:47:19 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    './*.{html,js}',            
+    './src/**/*.{html,js}',      
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+}
